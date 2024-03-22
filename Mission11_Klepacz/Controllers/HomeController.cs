7d0539df91ec78984fd73bb04694b82d326d91e5
Cameron Klepacz <cameron.klepacz@gmail.com>
2024-03-22 17:11:28 -0600

    Added Pagination and a basic view for each item in the DB

--- Mission11_Klepacz/Controllers/HomeController.cs
+++ Mission11_Klepacz/Controllers/HomeController.cs
@@ -1,14 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission11_Klepacz.Models;
+using Mission11_Klepacz.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Mission11_Klepacz.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IBookRepository _repo;
+        public HomeController(IBookRepository repo)
         {
-            return View();
+            _repo = repo;
+        }
+        public IActionResult Index(int pageNum)
+        {
+            int pageSize = 10;
+            var book = new BooksListViewModel
+            {
+                Books = _repo.Books
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+                }
+            };
+            return View(book);
         }
     }
 }
